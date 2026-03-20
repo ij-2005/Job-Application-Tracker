b8db1de8ff8e47411495baa2801df858fbb75d35
officialij2005@gmail.com <officialij2005@gmail.com>
2026-03-20 11:31:08 +0800

    did the frontEnd for the login page, fixing some files currently

---
+++ src/LoginPage.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+
+function LoginPage() {
+
+  const HandleLogin = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log(`"Email: ${email}`);
+    console.log(`Password: ${password}`);
+  };
+
+  const handleRegister = () => {
+    console.log("Register Clicked.");
+  }
+
+  return (
+    <>
+      <section id="center">
+
+        <div className="login-container">
+
+          <div className='titleHeader'> 
+          <h1>Job Application Tracker</h1>
+          <p className='subTitle'><i>by IJ2005</i></p>
+        </div>
+        <form className="login-form" onSubmit={HandleLogin}>
+          <div className="login-inputs">
+            <input type="email" name="email" placeholder="Enter email.." required />
+            <input type="password" name="password" placeholder="Enter password.." required />
+          </div>
+          <div className="login-buttons">
+            <button type="submit" className="signIn-button">Sign In</button>
+            <button type="button" className="register-button" onClick={handleRegister}>
+              Register
+            </button>
+          </div>
+        </form>
+
+        </div>
+        
+      </section>
+    </>
+  )
+}
+
+export default LoginPage
