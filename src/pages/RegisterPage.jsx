98c92afc39982d7c35c1436cdd2044dc7eb16e36
officialij2005@gmail.com <officialij2005@gmail.com>
2026-03-20 12:28:41 +0800

    Created the frontEnd for the RegisterPage with css also. No backend Logic yet.

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,13 +4,45 @@ function RegisterPage(){
 
     const navigate = useNavigate();
 
-    const BackButton = () => {
+    const handleBack = () => {
         navigate("/");
     }
+
+    const handleRegister = (event) => {
+    event.preventDefault();
+    console.log("Register Clicked.");
+    // later: send form data to backend API
+  };
+
     return (
         <>
-        <p>Hello this is register.</p>
-        <button onClick={BackButton}>Back</button>
+        <section id="center">
+        <div className="register-container">
+
+          <div className="titleHeader"> 
+            <h1>Nice to meet you!</h1>
+            <p className="subTitle"><i>by IJ2005</i></p>
+          </div>
+
+          <form className="register-form" onSubmit={handleRegister}>
+            <div className="register-inputs">
+              <input type="text" name="name" placeholder="Enter name.." required />
+              <input type="email" name="email" placeholder="Enter email.." required />
+              <input type="password" name="password" placeholder="Enter password.." required />
+              <input type="password" name="confirmPassword" placeholder="Confirm password.." required />
+            </div>
+            <div className="register-buttons">
+              <button type="button" className="back-button" onClick={handleBack}>
+                Back to Login Page
+              </button>
+              <button type="submit" className="register-button"> 
+                Register
+              </button>
+            </div>
+          </form>
+
+        </div>
+        </section>
         </>
     );
 }
