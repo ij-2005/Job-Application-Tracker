1a79c65a5c9cc9d770e6b941deb92f1ad4638108
officialij2005@gmail.com <officialij2005@gmail.com>
2026-03-20 11:45:09 +0800

    sorted files, not confusing now. Also added Routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App.jsx'
 import './App.css'
-import LoginPage from './LoginPage.jsx'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage />
+    <App />
   </StrictMode>,
 )
