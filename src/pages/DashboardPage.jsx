6861305ce6e4681d808415a0065c737334bfa793
officialij2005@gmail.com <officialij2005@gmail.com>
2026-03-24 21:20:10 +0800

    added a blank divs for the settlement of components in the Dashboard.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -3,12 +3,30 @@ import { useNavigate } from "react-router-dom";
 
 function DashboardPage(){
     const navigate = useNavigate();
-
+    
     const loginSite = () => {
         navigate("/");
     }
     return(
         <>
+        <section id="center">
+        <div className="dashboard-container">
+            <div className="titleHeader"> 
+                <h1>Job Tracker 101</h1>
+            </div>
+
+            <div className="componentsBar">
+                
+            </div>
+
+            <div className="applicationList">
+
+            </div>
+            
+        </div>
+
+        </section>
+
         <p>Hello this is the dashboard page.</p>
         <button onClick={loginSite}>Back home.</button>
         </>
